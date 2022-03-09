864ee5870373ffd1298f6c0efce9aab2b760023e
Piyush Pandey <piyushpvns@gmail.com>
2022-03-09 16:55:56 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container border bg-grey">
+    <div class="row">
+      <h1 class="text-center">ADMIN</h1>
+    </div>
+    <SearchUsers />
+    <UsersNew />
+  </div>
+</template>
+
+<script>
+import SearchUsers from "./components/SearchUsers";
+import UsersNew from "./components/UsersNew";
+export default {
+  name: "App",
+  components: {
+    SearchUsers,
+    UsersNew,
+  },
+
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style></style>
